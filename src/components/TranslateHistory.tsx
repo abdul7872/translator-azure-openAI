7fb69463022a405ebee386aa8e61bda09ca40144
Abdullah <aabdull7872@gmail.com>
2024-06-21 23:31:37 +0530

    Dev - add console log 2.

--- src/components/TranslateHistory.tsx
+++ src/components/TranslateHistory.tsx
@@ -16,10 +16,11 @@ async function TranslateHistory() {
     : process.env.VERCEL_URL
     }/api/translation-history?userId=${userId}`
 
-    console.log({historyURL})
 
   const response = await fetch(historyURL, { next: { tags: ['translationHistory'] } })
   const { translations }: UserType = await response.json();
+
+  console.log('qwe',{historyURL, translations})
 
   return (
     <div>
